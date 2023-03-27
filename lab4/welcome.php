73acf6c96192337d754fd6c2c7fbfea9138291a7
CedricPedric <cedricnelson03@gmail.com>
2023-03-27 15:18:59 +0200

    alles in 1 php file

--- lab4/welcome.php
+++ lab4/welcome.php
@@ -1,8 +1,4 @@
 <?php
-//    echo "<h2>your name = {$_POST["fname"]}</h2>";
-//    echo "<h2>your email = {$_POST["femail"]}</h2>";
-
-
     if ($_SERVER["REQUEST_METHOD"] == "GET") {
         if (empty($_GET["name"])) {
           $nameErr = "Name is required";
@@ -38,38 +34,38 @@ $nameErr = $emailErr  = "";
 $name = $email = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-  }
+    if (empty($_POST["name"])) {
+        $nameErr = "Name is required";
+    } else {
+        $name = test_input($_POST["name"]);
+    }
 
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-  }
-}
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+    } else {
+        $email = test_input($_POST["email"]);
+    }
+    }
 
 function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+    }
 ?>
 
 <h2>PHP Form Validation Example</h2>
 <p><span class="error">* required field</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  Name: <input type="text" name="name">
-  <span class="error">* <?php echo $nameErr;?></span>
-  <br><br>
-  E-mail: <input type="text" name="email">
-  <span class="error">* <?php echo $emailErr;?></span>
-  <br><br>
-  <input type="submit" name="submit" value="Submit">
-</form>
+    Name: <input type="text" name="name">
+    <span class="error">* <?php echo $nameErr;?></span>
+    <br><br>
+    E-mail: <input type="text" name="email">
+    <span class="error">* <?php echo $emailErr;?></span>
+    <br><br>
+    <input type="submit" name="submit" value="Submit">
+    </form>
 
 <?php
 echo "<h2>Your Input:</h2>";
